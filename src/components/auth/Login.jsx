c587637ac4f2e509a576d9e83cc93a7cf6495238
Agregor <121370651+shubhamsolo@users.noreply.github.com>
2024-12-27 15:00:35 +0530

    Update Login.jsx

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
         e.preventDefault();
         try {
             dispatch(setLoading(true));
-            const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
+            const res = await axios.post(`https://jobportalbackend-8jbj.onrender.com/${USER_API_END_POINT}/login`, input, {
                 headers: {
                     "Content-Type": "application/json"
                 },
